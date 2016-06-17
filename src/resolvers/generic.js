0c9431ac977deec715de10bf00990a164e0afe0c
Andrew Ingram <andy@andrewingram.net>
2016-06-17 12:22:17 +0100

    improve globalId resolver

--- src/resolvers/generic.js
+++ src/resolvers/generic.js
@@ -10,8 +10,14 @@ const alias =
 
 const globalId =
   (fn) =>
-  (obj, args, context, info) => toGlobalId(info.parentType.name, fn(obj));
-
+  (obj, args, context, info) => {
+    if (typeof fn === 'function') {
+      return toGlobalId(info.parentType.name, fn(obj));
+    } else if (typeof fn === 'string') {
+      return toGlobalId(info.parentType.name, obj[fn]);
+    }
+    return toGlobalId(info.parentType.name, obj[info.fieldName]);
+  };
 
 export {
   alias,
